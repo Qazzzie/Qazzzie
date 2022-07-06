e1abc702980ee43c23762c17f5e3cb842d5a6ebc
Zach Santangelo <santangelozach@gmail.com>
2022-07-05 23:23:19 -0700

    fixed image url

---
+++ js/boids/spawnBoids.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById('boids');
+// const canvas2DContext = canvas.getContext('2d'); 
+
+// https://stackoverflow.com/questions/5484578/how-to-get-document-height-and-width-without-using-jquery
+canvas.width = window.innerWidth || document.body.clientWidth
+canvas.height = window.innerHeight || document.body.clientHeight
+
+var boids = 50;
